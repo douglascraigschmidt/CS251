16ebe1d3f5d0d3aca3ef0e807656dc31d2a57f03
William R. Otte <wotte@isis.vanderbilt.edu>
2015-01-05 15:32:53 -0600

    Revised documentation.

--- assignments/assignment1/ugrad/src/main/java/edu/vanderbilt/eecs/cs251/CharArray.java
+++ assignments/assignment1/ugrad/src/main/java/edu/vanderbilt/eecs/cs251/CharArray.java
@@ -7,11 +7,9 @@ import java.util.Spliterator;
 import java.util.function.Consumer;
 
 /**
- * @class CharArray
- *
- * @brief An array wrapper that allows for resizeable arrays.
+ * Provides a wrapper around primitive char arrays, allowing for dynamic resizing.
  */
-class CharArray implements Comparable<CharArray>, Cloneable {
+public class CharArray implements Comparable<CharArray>, Cloneable {
     /**
      * The underlying array.
      */
@@ -25,14 +23,14 @@ class CharArray implements Comparable<CharArray>, Cloneable {
      */
 
     /**
-     * @brief Constructs an array of the given size.
+     * Constructs an array of the given size.
      * @param size Nonnegative integer size of the desired array.
      */
     public CharArray(int size) {
     }
 
     /**
-     * @brief Constructs an array of the given size, filled with the provided
+     * Constructs an array of the given size, filled with the provided
      * default value.
      * @param size Nonnegative integer size of the desired array.
      * @param mDefaultvalue A default value for the array.
@@ -41,7 +39,8 @@ class CharArray implements Comparable<CharArray>, Cloneable {
     }
 
     /**
-     * @brief Copy constructor.
+     * Copy constructor; creates a deep copy of the provided array.
+     * @param s The array to be copied.
      */
     public CharArray(CharArray s) {
     }
@@ -52,23 +51,23 @@ class CharArray implements Comparable<CharArray>, Cloneable {
     }
 
     /**
-     * @brief Provides the current size of the array.
+     * @return The current size of the array.
      */
     public int size() {
         return 0;
     }
 
     /**
-     * @brief Provides the current maximum capacity of the array withough
-     * requiring reallocation.
+     * @return The current maximum capacity of the array withough
      */
     public int capacity() {
         return 0;
     }
 
     /**
-     * @brief Resizes the array to the requested size.
-     * @details Changes the capacity of this array to hold the requested number of elements.
+     * Resizes the array to the requested size.
+     *
+     * Changes the capacity of this array to hold the requested number of elements.
      * Note the following optimizations/implementation details:
      * <ul>
      *   <li> If the requests size is smaller than the current maximum capacity, new memory
@@ -82,7 +81,7 @@ class CharArray implements Comparable<CharArray>, Cloneable {
     }
 
     /**
-     * @brief Returns the element at the requested index.
+     * @return the element at the requested index.
      * @param index Nonnegative index of the requested element.
      * @throws ArrayIndexOutOfBoundsException If the requested index is outside the
      * current bounds of the array.
@@ -92,7 +91,7 @@ class CharArray implements Comparable<CharArray>, Cloneable {
     }
 
     /**
-     * @brief Sets the element at the requested index with a provided value.
+     * Sets the element at the requested index with a provided value.
      * @param index Nonnegative index of the requested element.
      * @param value A provided value.
      * @throws ArrayIndexOutOfBoundsException If the requested index is outside the
@@ -106,7 +105,7 @@ class CharArray implements Comparable<CharArray>, Cloneable {
      * <p>
      * This is a requirement of the Comparable interface.  It is used to provide
      * an ordering for CharArray elements.
-     * @returns a negative value if the provided array is "less than" this array,
+     * @return a negative value if the provided array is "less than" this array,
      * zero if the arrays are identical, and a positive value if the
      * provided array is "greater than" this array.
      */
