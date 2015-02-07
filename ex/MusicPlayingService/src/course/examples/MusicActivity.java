372cd89466dacdf44a1704b52ead471d79201452
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-02-07 10:20:49 -0600

    Updates.

--- ex/MusicPlayingService/src/course/examples/MusicActivity.java
+++ ex/MusicPlayingService/src/course/examples/MusicActivity.java
@@ -4,14 +4,30 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * This MusicActivity is a simple front-end to the MusicService, which
  * plays a song in the background.
  */
 public class MusicActivity extends Activity {
+    /**
+     * Debug Tag for logging debug output to LogCat
+     */
+    private final String TAG =
+        MusicActivity.class.getSimpleName();
+
+    /**
+     * URL to the default song to play if the user doesn't enter a URL.
+     */
+    private static String DEFAULT_SONG = 
+        "http://www.dre.vanderbilt.edu/~schmidt/braincandy.m4a";
+
+    /**
+     * User's selection of URL to download.
+     */
+    private EditText mUrlEditText;
+
     /**
      * Hook method called when a new instance of Activity is created.
      * One time initialization code goes here, e.g., UI layout and
@@ -27,6 +43,10 @@ public class MusicActivity extends Activity {
 
         // Set the default layout.
         setContentView(R.layout.main);
+
+        // Cache the EditText resource. 
+        mUrlEditText = 
+            (EditText) findViewById(R.id.mUrlEditText);
     }
 	
     /**
@@ -38,7 +58,7 @@ public class MusicActivity extends Activity {
         // song!)
         Intent intent =
             MusicService.makeIntent(this,
-                                    R.raw.braincandy);
+                                    getUrlString());
 
         // Start the MusicService.
         startService(intent);
@@ -51,9 +71,25 @@ public class MusicActivity extends Activity {
         // Create an intent that will stop the MusicService.
         Intent intent =
             MusicService.makeIntent(this,
-                                    0);
+                                    null);
 
         // Stop the MusicService.
         stopService (intent);
     }	
+
+    /**
+     * Read the URL EditText and return the String it contains.
+     * 
+     * @return String value in mUrlEditText
+     */
+    public String getUrlString() {
+        // Get the user input (if any).
+        String url = mUrlEditText.getText().toString();
+
+        //  Use the default URL if the user doesn't supply one.
+        if (url.equals(""))
+            url = DEFAULT_SONG;
+
+        return url; 
+    }
 }
--- ex/MusicPlayingService/src/course/examples/MusicService.java
+++ ex/MusicPlayingService/src/course/examples/MusicService.java
@@ -1,23 +1,31 @@
 package course.examples;
 
+import java.io.IOException;
+
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.util.Log;
 
 /**
- * This MusicService uses a MediaPlayer to play a song in the
- * background.
+ * This MusicService uses a MediaPlayer to download and play a song in
+ * the background.
  */
-public class MusicService extends Service {
+public class MusicService extends Service 
+                          implements MediaPlayer.OnPreparedListener {
     /**
      * Debugging tag used by the Android logger.
      */
     private String TAG = getClass().getSimpleName();
 
-    private static String SONG_ID = "SongID";
+    /**
+     * Key used to identify the extra in an intent that contains the
+     * URL for the requested song.
+     */
+    private static String SONG_URL = "SONG_URL";
 
     /**
      * The MediaPlayer that plays a song in the background.
@@ -26,22 +34,35 @@ public class MusicService extends Service {
 
     /**
      * This factory method returns an intent used to play and stop
-     * playing a song, which is designated by the @a resId.
+     * playing a song, which is designated by the @a songURL.
      */
     public static Intent makeIntent(final Context context,
-                                    int resId) {
+                                    String songURL) {
         // Create an intent that points to the MusicService.
         Intent intent =
             new Intent(context,
                        MusicService.class);
 
-        if (resId != 0) 
-            // Add the SONG_ID as an "extra" to the intent.
-            intent.putExtra(SONG_ID,
-                            resId);
+        // Add the SONG_URL as an "extra" to the intent.
+        intent.putExtra(SONG_URL,
+                        songURL);
         return intent;
     }
 
+    /**
+     * Hook method called when a new instance of Service is created.
+     * One time initialization code goes here.
+     */
+    @Override
+    public void onCreate() {
+        // Always call super class for necessary
+        // initialization/implementation.
+        super.onCreate();
+
+        // Create a MediaPlayer that will play the requested song.
+        mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    }
     /**
      * Hook method called every time the MusicService is started.
      */
@@ -52,23 +73,36 @@ public class MusicService extends Service {
         Log.i(TAG,"onStartCommand() entered");
 
         // Extract the resId for the requested song from the â€œextraâ€�.
-        int songId = intent.getIntExtra(SONG_ID, 0);
+        String songURL = intent.getStringExtra(SONG_URL);
+        try {
+            // Indicate the URL indicating the song to play.
+            mPlayer.setDataSource(songURL);
+                
+            // Register the callback.
+            mPlayer.setOnPreparedListener(this);
 
-        // Create a MediaPlayer that will play the requested song.
-        mPlayer = MediaPlayer.create(this,
-                                     songId);
+            // This call doesn't block the UI Thread.
+            mPlayer.prepareAsync(); 
+        } catch (IOException e) {
+        	e.printStackTrace();
+        }
 
-        // Just play the song once, rather than have it loop
-        // endlessly.
-        mPlayer.setLooping(false);
-
-        // Start playing the song.
-        mPlayer.start();
-
-        // Donâ€™t restart Service if it shuts down
+        // Don't restart Service if it shuts down
         return START_NOT_STICKY;
     }
-	
+
+    /** 
+     * Called back when MediaPlayer is ready.
+     */
+    public void onPrepared(MediaPlayer player) {
+        // Just play the song once, rather than have it loop
+        // endlessly.
+        player.setLooping(false);
+
+        // Start playing the song.
+        player.start();
+    }
+
     /**
      * Hook method called when the MusicService is stopped.
      */
