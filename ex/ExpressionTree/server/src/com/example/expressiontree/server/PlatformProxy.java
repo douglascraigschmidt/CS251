ed698813e0daa1dfe5e9a0bcc86d455645eb80f3
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2015-02-19 07:47:50 -0600

    Updates and comments for Mitchell.

--- ex/ExpressionTree/server/src/com/example/expressiontree/server/PlatformProxy.java
+++ ex/ExpressionTree/server/src/com/example/expressiontree/server/PlatformProxy.java
@@ -11,157 +11,176 @@ import com.example.expressiontree.model.ServerResponse;
 /**
  * @class PlatformProxy
  * 
- * @brief Handles marshalling calls to the Platform interface into instances of
- *        ServerResponse, which can then be converted into GSON and sent back to
- *        the client.
+ * @brief Handles marshalling calls to the Platform interface into
+ *        instances of ServerResponse, which can then be converted
+ *        into GSON and sent back to the client.
  * 
- *        This class also uses Thread specific storage to avoid collisions when
- *        processing requests from multiple threads at once.
+ *        This class also uses the Thread-Specific Storage pattern to
+ *        avoid collisions when processing requests from multiple
+ *        threads at once.
  */
 public class PlatformProxy extends Platform {
+    /**
+     * The list of platform calls made to this class. Each thread has
+     * it's own list.
 
-	/**
-	 * The list of platform calls made to this class. Each thread has it's own list
-	 */
-	Map<Long, List<PlatformCall>> mCallObjectMap = new ConcurrentHashMap<>();
+     * @@ Mitchell, can you please try to use the Java ThreadLocal
+     * storage class for this since it does essentially the same
+     * thing, only more efficiently.
+     */
+    Map<Long, List<PlatformCall>> mCallObjectMap = new ConcurrentHashMap<>();
 	
-	/**
-	 * Returns the JSON object that was built since reap was last called or
-	 * since the Platform was initialized.
-	 */
-	public ServerResponse reap() {
+    /**
+     * Returns the JSON object that was built since reap() was last
+     * called or since the Platform was initialized.
+     */
+    // @@ Mitchell, why is this method called "reap"?
+    public ServerResponse reap() {
+        // @@ Mitchell, could we please use the Java ThreadLocal
+        // storage class for this stuff?
+
+        // Get the calls made by the current thread.
+        long threadId = Thread.currentThread().getId(); 
+        List<PlatformCall> callObjects =
+            mCallObjectMap.get(threadId);		
 		
-		// Get the calls made by the current thread.
-		long threadId = Thread.currentThread().getId(); 
-		List<PlatformCall> callObjects = mCallObjectMap.get(threadId);		
+        // Construct a response
+        ServerResponse resp = new ServerResponse();
+
+        resp.setResult("ok");
+        resp.setPlatformCalls(callObjects);
+
+        // Restart collecting call objects for this thread.
+        mCallObjectMap.remove(threadId);
+
+        return resp;
+    }
+
+    /**
+     * Takes a function name and several params and appends it to the
+     * JSON object.
+     */
+    private void appendCallObject(String fname, String... params) {
+        // @@ Mitchell, could we please use the Java ThreadLocal
+        // storage class for this stuff?
+
+        // Get the thread-specific storage object
+        long threadId = Thread.currentThread().getId(); 
+        List<PlatformCall> callObjects =
+            mCallObjectMap.get(threadId);
 		
-		// Construct a response
-		ServerResponse resp = new ServerResponse();
-
-		resp.setResult("ok");
-		resp.setPlatformCalls(callObjects);
-
-		// Restart collecting call objects for this thread.
-		mCallObjectMap.remove(threadId);
-
-		return resp;
-	}
-
-	/**
-	 * Takes a function name and several params and appends it to the JSON
-	 * object.
-	 */
-	private void appendCallObject(String fname, String... params) {
-		// Get the thread specific storage object
-		long threadId = Thread.currentThread().getId(); 
-		List<PlatformCall> callObjects = mCallObjectMap.get(threadId);
+        if (callObjects == null) {
+            callObjects = new ArrayList<>();
+            mCallObjectMap.put(threadId, callObjects);
+        }
 		
-		if (callObjects == null) {
-			callObjects = new ArrayList<>();
-			mCallObjectMap.put(threadId, callObjects);
-		}
-		
-		List<String> paramList = new ArrayList<String>();
+        List<String> paramList = new ArrayList<String>();
 
-		for (String p : params)
-			paramList.add(p);
+        for (String p : params)
+            paramList.add(p);
 
-		callObjects.add(new PlatformCall(fname, paramList));
-	}
+        callObjects.add(new PlatformCall(fname,
+                                         paramList));
+    }
 
-	/**
-	 * Appends a call to outputLine to the current list of calls to be returned
-	 * to the client.
-	 */
-	@Override
+    /**
+     * Appends a call to outputLine to the current list of calls to be
+     * returned to the client.
+     */
+    @Override
 	public String outputLine(String line) {
-		appendCallObject("outputLine", line);
-		return null;
-	}
+        appendCallObject("outputLine", line);
+        // @@ Mitchell, should this method return the line parameter?
+        return null;
+    }
 
-	/**
-	 * Since all input is handled by the servlet, this method will never be
-	 * called.
-	 */
-	@Override
+    /**
+     * Since all input is handled by the servlet, this method will
+     * never be called.
+     */
+    @Override
 	public String retrieveInput(boolean verbose) {
-		// This method will not be used by the server.
-		return null;
-	}
+        // This method will not be used by the server.
+        return null;
+    }
 
-	/**
-	 * Appends a call to outputString to the current list of calls to be
-	 * returned to the client.
-	 */
-	@Override
+    /**
+     * Appends a call to outputString to the current list of calls to
+     * be returned to the client.
+     */
+    @Override
 	public String outputString(String input) {
-		appendCallObject("outputString", input);
-		return null;
-	}
+        appendCallObject("outputString", input);
+        // @@ Mitchell, should this method return the line parameter?
+        return null;
+    }
 
-	/**
-	 * Returns the name of the current platform. Since we don't know what the
-	 * client's platform is, we're returning proxy.
-	 */
-	@Override
-	public String platformName() {
-		return "Proxy";
-	}
+    /**
+     * Returns the name of the current platform. Since we don't know what the
+     * client's platform is, we're returning proxy.
+     */
+    @Override
+    public String platformName() {
+        return "Proxy";
+    }
 
-	/**
-	 * This method is only called by the Android application and will never be
-	 * called in the context of this server.
-	 */
-	@Override
+    /**
+     * This method is only called by the Android application and will
+     * never be called in the context of this server.
+     */
+    @Override
 	public boolean isCommandLinePlatform() {
-		// This method will not be used by the server.
-		return false;
-	}
+        // This method will not be used by the server.
+        return false;
+    }
 
-	/**
-	 * Appends a call to outputMenu to the current list of calls to be returned
-	 * to the client.
-	 */
-	@Override
-	public void outputMenu(String numeral, String option, String selection) {
-		appendCallObject("outputMenu", numeral, option, selection);
-	}
+    /**
+     * Appends a call to outputMenu to the current list of calls to be
+     * returned to the client.
+     */
+    @Override
+	public void outputMenu(String numeral,
+                               String option,
+                               String selection) {
+        appendCallObject("outputMenu", numeral, option, selection);
+    }
 
-	/**
-	 * Appends a call to enableOption to the current list of calls to be
-	 * returned to the client.
-	 */
-	@Override
+    /**
+     * Appends a call to enableOption to the current list of calls to
+     * be returned to the client.
+     */
+    @Override
 	public void enableOption(String option) {
-		appendCallObject("enableOption", option);
+        appendCallObject("enableOption", option);
 
-	}
+    }
 
-	/**
-	 * Appends a call to disableAll to the current list of calls to be returned
-	 * to the client.
-	 */
-	@Override
+    /**
+     * Appends a call to disableAll to the current list of calls to be
+     * returned to the client.
+     */
+    @Override
 	public void disableAll(boolean verbose) {
-		appendCallObject("disableAll", Boolean.toString(verbose));
-	}
+        appendCallObject("disableAll", Boolean.toString(verbose));
+    }
 
-	/**
-	 * Appends a call to addString to the current list of calls to be returned
-	 * to the client.
-	 */
-	@Override
+    /**
+     * Appends a call to addString to the current list of calls to be
+     * returned to the client.
+     */
+    @Override
 	public String addString(String Input) {
-		appendCallObject("addString", Input);
-		return null;
-	}
+        appendCallObject("addString", Input);
+        return null;
+    }
 
-	/**
-	 * Appends a call to errorLog to the current list of calls to be returned to
-	 * the client.
-	 */
-	@Override
-	public void errorLog(String javaFile, String errorMessage) {
-		appendCallObject("errorLog", javaFile, errorMessage);
-	}
+    /**
+     * Appends a call to errorLog to the current list of calls to be
+     * returned to the client.
+     */
+    @Override
+	public void errorLog(String javaFile,
+                             String errorMessage) {
+        appendCallObject("errorLog", javaFile, errorMessage);
+    }
 }
--- ex/ExpressionTree/server/src/com/example/expressiontree/server/ServerInputHandler.java
+++ ex/ExpressionTree/server/src/com/example/expressiontree/server/ServerInputHandler.java
@@ -17,107 +17,116 @@ import com.google.gson.Gson;
 /**
  * @class ServerInputHandler
  *
- * @brief Handles GET requests coming into the server
- * @see
+ * @brief Handles GET requests from the client to the server.
  */
 @WebServlet("/")
 public class ServerInputHandler extends HttpServlet {
-	/**
-	 * Version number.
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * Version number.
+     */
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * Classifies the Handler as verbose.
-	 */
-	static boolean mVerboseField;
+    /**
+     * Classifies the Handler as verbose.
+     */
+    static boolean mVerboseField;
 
-	/**
-	 * A factory for making user commands. Each factory contains it's own
-	 * context, where the expression tree state resides. Each client gets their
-	 * own.
-	 */
-	Map<Long, UserCommandFactory> mClientStates;
+    /**
+     * A factory for making user commands. Each factory contains it's
+     * own context, where the expression tree state resides. Each
+     * client gets their own.
+     */
+    Map<Long, UserCommandFactory> mClientStates;
 	
-	/**
-	 * The next client ID we're going to assign
-	 */
-	long mNextID = 0;
+    /**
+     * The next client ID we're going to assign
+     */
+    long mNextID = 0;
 
-	/** Constructor. */
-	@Override
-	public void init() throws ServletException {
-		mClientStates = new HashMap<>();
-		Platform.instance(new PlatformProxy());
-	}
+    /** 
+     * Virtual constructor. 
+     */
+    @Override
+    public void init() throws ServletException {
+        mClientStates = new HashMap<>();
+        Platform.instance(new PlatformProxy());
+    }
 
-	/**
-	 * This method is called back when input is available. It implements the
-	 * Template Method pattern to perform the sequence of steps associated with
-	 * processing expression tree application commands.
-	 */
-	@Override
+    /**
+     * This method is called back when input is available.  It
+     * implements the Template Method pattern to perform the sequence
+     * of steps associated with processing expression tree application
+     * commands.
+     */
+    @Override
 	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {		
-		// Retrieves input from user.
-		String userInputCommand = request.getParameter("input");
+                             HttpServletResponse response) throws ServletException, IOException {		
+        // Retrieves input from user.
+        String userInputCommand = request.getParameter("input");
 
-		if (userInputCommand == null) {
-			response.getWriter().println("{result: \"malformed\"}");
-		} else {
-			// URL decode the string.
-			userInputCommand = URLDecoder.decode(userInputCommand, "UTF-8");
+        if (userInputCommand == null) {
+            response.getWriter().println("{result: \"malformed\"}");
+        } else {
+            // URL decode the string.
+            userInputCommand =
+                URLDecoder.decode(userInputCommand, "UTF-8");
 				
-			// See if they gave us an ID
-			String id = request.getParameter("clientID");
-			boolean foundId = false;
-			UserCommandFactory factory;
+            // See if they gave us an ID.
+            String id = request.getParameter("clientID");
+            boolean foundId = false;
+            UserCommandFactory factory;
 			
-			// If they did, and it's in our map, use that state
-			if (id != null && mClientStates.containsKey(Long.parseLong(id))) {
-				factory = mClientStates.get(Long.parseLong(id));
-				foundId = true;
-			}
-			else
-				factory = new UserCommandFactory (new TreeOps());
+            // If they did--and it's in our map--use that state.
+            if (id != null && mClientStates.containsKey(Long.parseLong(id))) {
+                factory = mClientStates.get(Long.parseLong(id));
+                foundId = true;
+            }
+            else
+                factory = new UserCommandFactory (new TreeOps());
 			
-			// Make the command
-			UserCommand command = factory.makeUserCommand(userInputCommand);
+            // Use the factory to make the command.
+            UserCommand command =
+                factory.makeUserCommand(userInputCommand);
 
-			// Call a hook method to execute the command.
-			try {
-				executeCommand(command);
+            // Call a hook method to execute the command.
+            try {
+                executeCommand(command);
 
-				command.printValidCommands(true);
-			} catch (Exception e) {
-				response.getWriter().print(
-						"{result: \"exception\", message: \"" + e.getMessage()
-								+ "\"}");
-				response.flushBuffer();
-				return;
-			}
+                command.printValidCommands(true);
+            } catch (Exception e) {
+                response.getWriter().print("{result: \"exception\", message: \"" 
+                                           + e.getMessage()
+                                           + "\"}");
+                response.flushBuffer();
+                return;
+            }
 			
-			ServerResponse resp = ((PlatformProxy) Platform.instance()).reap();
-			
-			if (!foundId) {
-				resp.setID(mNextID);
-				mClientStates.put(mNextID, factory);
-				++mNextID;
-			}
-			
-			// Convert the Server Response into JSON using GSON and write it out.
-			Gson gson = new Gson();
-			String json = gson.toJson(resp);
-			response.getWriter().println(json);
-		}
+            // @@ Mitchell, please explain what this call is doing
+            // (I'm confused about why it's called "reap").
+            ServerResponse resp =
+                ((PlatformProxy) Platform.instance()).reap();
 
-		response.flushBuffer();
-	}
+            // @@ Mitchell, please document what this code is doing.
+            if (!foundId) {
+                resp.setID(mNextID);
+                mClientStates.put(mNextID, factory);
+                ++mNextID;
+            }
+			
+            // Convert the Server Response into JSON using GSON and
+            // write it out.
+            Gson gson = new Gson();
+            String json = gson.toJson(resp);
+            response.getWriter().println(json);
+        }
 
-	/**
-	 * This hook method executes a command.
-	 */
-	private void executeCommand(UserCommand command) throws Exception {
-		command.execute();
-	}
+        response.flushBuffer();
+    }
+
+    /**
+     * This hook method executes a command.
+     */
+    private void executeCommand(UserCommand command) throws Exception {
+        command.execute();
+    }
 }
