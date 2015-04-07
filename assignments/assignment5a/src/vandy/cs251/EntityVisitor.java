6f0fbf355512aa0a5faaf527430ce2f97d4944b8
William R. Otte <wotte@isis.vanderbilt.edu>
2015-04-06 19:22:26 -0500

    Remove accidental edit.

--- assignments/assignment5a/src/vandy/cs251/EntityVisitor.java
+++ assignments/assignment5a/src/vandy/cs251/EntityVisitor.java
@@ -6,6 +6,4 @@ import vandy.cs251.Entity;
 public interface EntityVisitor {
   /** Visit the provided Entity. */
   public void visit(Entity entity);
-  public void visit(OtherEntity entity);
-  public void visit(MyCoolOtherEntity entity);
 }
