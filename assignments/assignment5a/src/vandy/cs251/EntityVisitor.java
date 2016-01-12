599cc5f3da6ec5c335f1401e52f7e8e23a038a3f
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2016-01-11 20:17:36 -0600

    Updates.

--- assignments/assignment5a/src/vandy/cs251/EntityVisitor.java
+++
@@ -1,9 +0,0 @@
-package vandy.cs251;
-
-import vandy.cs251.Entity;
-
-/** This class impelments the Visitor design pattern for Entity objects. */
-public interface EntityVisitor {
-  /** Visit the provided Entity. */
-  public void visit(Entity entity);
-}
--- assignments/assignment5b/src/edu/vanderbilt/a4_android/ui/Spliterator.java
+++
@@ -1,9 +0,0 @@
-package edu.vanderbilt.a4_android.ui;
-
-import edu.vanderbilt.a4_android.ui.EntityVisitor;
-
-interface Spliterator<T> {
-  public long estimateSize();
-  public boolean tryAdvance(EntityVisitor visitor);
-  public Spliterator<T> trySplit();
-}
