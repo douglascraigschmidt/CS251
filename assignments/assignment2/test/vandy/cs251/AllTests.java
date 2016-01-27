eb5364e530742972cdf106fccff22f3f90b18c0e
William R. Otte <wotte@dre.vanderbilt.edu>
2016-01-27 14:34:41 -0600

    Assignment 2 Skeleton

---
+++ assignments/assignment2/test/vandy/cs251/AllTests.java
@@ -0,0 +1,10 @@
+package vandy.cs251;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ListArrayCharacterTest.class})
+public class AllTests {
+}
