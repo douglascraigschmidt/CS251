dabacc10cbf2eabe753a9665ef7c0d44219516cc
douglascraigschmidt <douglas.craig.schmidt@gmail.com>
2021-07-11 07:13:16 -0500

    updates

--- assignment5/tests/main.cpp
+++
@@ -1,9 +0,0 @@
-/* @author G. Hemingway, copyright 2020 - All rights reserved */
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv)
-{
-    // Run the registered tests
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
