475afb81dc1f0f273bd09f350dcfec063eae1dc3
douglascraigschmidt <douglas.craig.schmidt@gmail.com>
2021-07-17 10:36:18 -0500

    updates

---
+++ assignment2/tests/moreTests.cpp
@@ -0,0 +1,34 @@
+// @author G. Hemingway, copyright 2020 - All rights reserved
+//
+#include "AllocationTracker.h"
+#include <gtest/gtest.h>
+
+namespace {
+// More testing for ScopedArray class.
+class MoreTests : public ::testing::Test {
+};
+
+TEST_F(MoreTests, Test1)
+{
+}
+
+TEST_F(MoreTests, Test2)
+{
+}
+
+TEST_F(MoreTests, Test3)
+{
+}
+
+TEST_F(MoreTests, Test4)
+{
+}
+
+TEST_F(MoreTests, Test5)
+{
+}
+
+TEST_F(MoreTests, Test6)
+{
+}
+}
